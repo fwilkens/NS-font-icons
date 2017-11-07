2ca3f649db7c9ab0f48168557c3433fbeb69acb2
Fletcher Wilkens <fletcher.wilkens1@gmail.com>
2017-11-07 15:38:03 -0800

    use standard icomoon free pack

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,8 @@ import { Component } from "@angular/core";
   template: `
     <ActionBar title="My App" class="action-bar"></ActionBar>
     <StackLayout>
-      <Label text="&#xe902;" class="icomoon my-icon"></Label>
+      <Label text="&#xe909;" class="icomoon my-icon"></Label>
     </StackLayout>
   `
 })
-export class AppComponent {
-  // Your TypeScript logic goes here
-}
+export class AppComponent {}
